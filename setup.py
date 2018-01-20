534801c528b06bb5084b0ec3dfffb8a6c1704abf
Basil Shubin <basil.shubin@gmail.com>
2018-01-20 11:45:53 +0700

    bump version number

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
 
     install_requires=[
         'django-fluent-contents>=1.0',
-        'fluentcms-emailtemplates',
+        'fluentcms-emailtemplates>0.2.5',
         'swapper',
     ],
     requires=[
